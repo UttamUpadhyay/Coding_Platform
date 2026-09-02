c2d0958cb7ce9e73c78c89663af453e6648bade3
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-02 21:51:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.24%) - CodeHub

---
+++ leetcode/0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int totalMoves = m + n - 2;
+        int downMoves = min(m - 1, n - 1);
+        long long result = 1;
+        
+        for (int i = 1; i <= downMoves; ++i) {
+            result = result * (totalMoves - downMoves + i) / i;
+        }
+        
+        return (int)result;
+    }
+};
