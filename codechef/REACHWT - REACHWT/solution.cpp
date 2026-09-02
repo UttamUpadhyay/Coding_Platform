ae42d130150d9ddb15fc95915a1687ae16a453dc
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-02 20:54:27 +0530

    Add CodeChef REACHWT - REACHWT

---
+++ codechef/REACHWT - REACHWT/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int N;
+    cin >> N;
+    if (N % 2 == 0) {
+      cout << (N / 2) * 30 << endl;
+    }
+    else {
+      cout << (N / 2) * 30 + (N / 3) * 20 << endl;
+    }
+  }
+
+}
+endl local
+friend keyword
+lower_bound snippet
+upper_bound snippet
