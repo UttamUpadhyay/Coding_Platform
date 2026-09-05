97a14d0411f4dfeb0cbcfa622dcc2093820b922e
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-05 20:02:59 +0530

    Add CodeChef MAYOR_PARTY - Welcome to the CodeChef AI Tutor

---
+++ codechef/MAYOR_PARTY - Welcome to the CodeChef AI Tutor/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int t;
+ cin >> t;
+ while (t--) {
+   int a, b, c;
+   cin >> a >> b >> c;
+   int sum = a + c;
+   cout << max(b, sum) << endl;
+ }
+
+}
+
+sum local
+shuffle snippet
+struct keyword
+set_union snippet
