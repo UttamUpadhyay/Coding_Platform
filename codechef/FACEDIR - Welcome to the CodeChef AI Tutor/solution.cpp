38a4956e7677b972fdccc08fa68438c1ac6c97cd
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-05 19:37:17 +0530

    Add CodeChef FACEDIR - Welcome to the CodeChef AI Tutor

---
+++ codechef/FACEDIR - Welcome to the CodeChef AI Tutor/solution.cpp
@@ -0,0 +1,25 @@
+cin >> t;
+  while (t--) {
+    int x;
+    cin >> x;
+    int dir = x % 4;
+    if (dir == 1 ){
+      cout << "East
+"; 
+    }
+    else if (dir == 2) {
+      cout << "South
+";
+    }
+    else if (dir == 3) {
+      cout << "West
+";
+    }
+    else {
+      cout << "North
+";
+    }
+  }
+
+dir local
+set_difference snippet
