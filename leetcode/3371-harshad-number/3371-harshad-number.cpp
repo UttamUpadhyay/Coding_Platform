82430f98b57fd0ee877e5862efa4126302b017c2
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-07 23:44:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.36%) - CodeHub

---
+++ leetcode/3371-harshad-number/3371-harshad-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int num = x;
+        while (x != 0) {
+            int rem = x % 10;
+            sum += rem;
+            x /= 10;
+        }
+        if (num % sum == 0) {
+            return sum;
+        }
+        else {
+            return -1;
+        }
+    }
+};
