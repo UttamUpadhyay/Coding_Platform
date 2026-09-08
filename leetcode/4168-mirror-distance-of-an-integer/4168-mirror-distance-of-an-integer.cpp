16bb54c0fde8bbf2a048c37ef63c22194e2da0a2
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-08 23:31:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.43%) - CodeHub

---
+++ leetcode/4168-mirror-distance-of-an-integer/4168-mirror-distance-of-an-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int rev = 0;
+        int num = n;
+        while (n != 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n /= 10;
+        }
+        return abs(num - rev);
+    }
+};
