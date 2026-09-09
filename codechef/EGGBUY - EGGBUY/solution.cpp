18c9d0ef6b7d3ea0195e12d975dfa81187364cb7
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-09 20:53:03 +0530

    Add CodeChef EGGBUY - EGGBUY

---
+++ codechef/EGGBUY - EGGBUY/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, y, f;
+  cin >> x >> y >> f;
+  cout << min(x * 12, (y * 12) + f) << endl;
+
+}
+
+friend keyword
+lower_bound snippet
+upper_bound snippet
