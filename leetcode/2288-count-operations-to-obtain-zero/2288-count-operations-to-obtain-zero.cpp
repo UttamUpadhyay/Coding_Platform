47ec447a74033be33997d6621a74a5708e976667
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-09 09:36:11 +0530

    Time: 6 ms (0.38%), Space: 9.3 MB (2.09%) - CodeHub

--- leetcode/2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ leetcode/2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        long count = 0;
-        if (num1 == 0 || num2 == 0) {
-            return count;
+        if (num1 ==0 || num2 == 0) {
+            return 0;
         }
-        else{
-        while (num1 != 0 || num1 != 0) {
-            if (num1 >= num2) {
-                num1 -= num2;
-                count++;
-            }
-            else {
-                num2 -= num1;
-                count++;
-            }
-        }}
-        return count;
+        if (num1 >= num2) {
+            return 1 + countOperations(num1 - num2, num2);
+        }
+        return 1 + countOperations(num1, num2 - num1);
     }
 };
