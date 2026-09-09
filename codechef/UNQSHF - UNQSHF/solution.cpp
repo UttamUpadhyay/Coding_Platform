6fd970578fd417a525c4dd6204e7ad2e8e6351ef
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-09 21:00:46 +0530

    Add CodeChef UNQSHF - UNQSHF

---
+++ codechef/UNQSHF - UNQSHF/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    string a, b;
+    for (int i = 0; i < n; i++) {
+      if (a[i] != b[i]) {
+        cout << "No"
+      }
+    }
+  }
+
+}
+
+noexcept keyword
+none_of snippet
+not keyword
+not_eq keyword
+alignof keyword
+union keyword
+any_of snippet
+next_permutation snippet
+transform snippet
