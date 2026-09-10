400124423934ee2135ccd4f57c07c550a5136bc6
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-10 15:11:05 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.09%) - CodeHub

--- leetcode/0062-unique-paths/0062-unique-paths.cpp
+++ leetcode/0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+int Helper(int i, int j, vector<vector<int>>&data) {
+	  if (i == 0 && j == 0) {
+	    return 1;
+	  }
+	  if (i < 0 || j < 0){
+	    return 0;
+	  }
+	  if (data[i][j] == 0) {
+	  data[i][j] =  Helper(i - 1, j, data) + Helper(i, j - 1, data);
+	  }
+	  return data[i][j];
+	  }
+	  
     int uniquePaths(int m, int n) {
-        int totalMoves = m + n - 2;
-        int downMoves = min(m - 1, n - 1);
-        long long result = 1;
-        
-        for (int i = 1; i <= downMoves; ++i) {
-            result = result * (totalMoves - downMoves + i) / i;
-        }
-        
-        return (int)result;
+        vector<vector<int>>dat(m, vector<int>(n));
+  return Helper(m - 1, n - 1, dat);
+
     }
 };
