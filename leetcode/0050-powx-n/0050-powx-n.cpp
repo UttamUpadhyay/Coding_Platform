a725b0892b68baafc4e1a796a7b24b51179532bb
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-14 14:33:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (8.1%) - CodeHub

--- leetcode/0050-powx-n/0050-powx-n.cpp
+++ leetcode/0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double y;
-        y = pow(x, n);
-        return y;
+        // if (n == 0) {
+        //     return 1;
+        // }
+        // else if (n > 0) {
+        // double y =  x * myPow(x, n - 1);
+        //  return y;
+        // }
+        return pow(x, n);
+       
     }
 };
