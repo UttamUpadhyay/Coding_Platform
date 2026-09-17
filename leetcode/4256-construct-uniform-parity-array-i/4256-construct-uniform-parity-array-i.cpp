f2b0243be0389cbe925d71929fd0eb0a61958111
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-17 23:23:42 +0530

    Time: 0 ms (100%), Space: 30.3 MB (66.95%) - CodeHub

---
+++ leetcode/4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        vector<int>nums2;
+        return true;
+        // for (int i = 0; i < nums1.size(); i++) {
+        //     nums2.push_back(nums1[i] - nums[i])
+        // }
+    }
+};
