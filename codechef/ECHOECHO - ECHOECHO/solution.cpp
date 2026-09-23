7449240de1f26985516f750060d315a6e485a9e7
UTTAM UPADHYAY <upadhyayuttam197@gmail.com>
2026-09-23 20:18:22 +0530

    Add CodeChef ECHOECHO - ECHOECHO

---
+++ codechef/ECHOECHO - ECHOECHO/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ string S;
+ cin >> S;
+if (S[0] == S[2] && S[1] == S[3]) {
+  cout << "Yes" << endl;
+}
+else {
+  cout << "No" << endl;
+}
+
+}
+
+string local
+std local
+stdc local
+set snippet
+set_difference snippet
+set_intersection snippet
+set_union snippet
+short keyword
+shuffle snippet
